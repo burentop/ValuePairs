239f171a0c04c69f99a8dee996e92c5afb56f2e3
Brent Perry <burentoperii@gmail.com>
2016-07-07 15:32:48 -0500

    Initial commit of ValuePairs.

--- ValuePairs/main.cpp
+++ ValuePairs/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    int val1 = 1;
+    int val2 = 1;
+    bool status = true;
+    
+    cout << "Please enter two whole numbers separated by a space (enter '\' to quit): ";
+    cin >> val1 >> val2;
+    cout << val1 << " " << val2 << "\n";
+    
+    while (status) {
+        cout << "Please enter two whole numbers separated by a space (enter '\' to quit): ";
+        cin >> val1 >> val2;
+        if (cin.fail()) {
+            cout << "Exiting\n";
+            status = false;
+        }
+        cout << val1 << " " << val2 << "\n";
+    }
 }
