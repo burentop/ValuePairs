9d022f69254833c4f7607e730601a1878625ba9e
Brent Perry <burentoperii@gmail.com>
2016-07-07 14:59:46 -0500

    Initial Commit

---
+++ ValuePairs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ValuePairs
+//
+//  Created by Brent Perry on 7/7/16.
+//  Copyright © 2016 Brent Perry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
