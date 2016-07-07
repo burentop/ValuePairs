81b68e5696827c3750356f099bbed86657cf5427
Brent Perry <burentoperii@gmail.com>
2016-07-07 17:11:35 -0500

    Add functionality to allow measurement units, and convert all to cm.

--- ValuePairs/main.cpp
+++ ValuePairs/main.cpp
@@ -24,37 +24,64 @@ bool find_largest(double x, double y)
     return false;
 }
 
+double convert(double input, string unit) {
+    constexpr double inch_to_cm = 2.54;
+    constexpr double meter_to_cm = 100.0;
+    constexpr double feet_to_cm = 30.54;
+    
+    double new_input = 0;
+    
+    if (unit == "in") {
+        new_input = input * inch_to_cm;
+    } else if (unit == "m") {
+        new_input = input * meter_to_cm;
+    } else if (unit == "ft") {
+        new_input = input * feet_to_cm;
+    } else {
+        new_input = input;
+    }
+    
+    return new_input;
+}
+
 
 int main()
 {
-    double input;
+    double input, converted;
+    string unit;
     double low, high;
     bool status = true;
     
-    cout << "Please enter a decimal number (enter '|' to quit): ";
-    cin >> input;
-    low = high = input;
-    cout << input << ", the only number entered so far.\n";
+    
+    
+    cout << "Please enter a decimal number and a unit ['cm', 'm', 'in', 'ft'](enter '|' to quit): ";
+    cin >> input >> unit;
+    
+    converted = convert(input, unit);
+    
+    low = high = converted;
+    cout << input << unit << " is " << converted << "cm, the only number entered so far.\n";
 
     
     while (status) {
-        cout << "Please enter another decimal number (enter '|' to quit): ";
-        cin >> input;
+        cout << "Please enter another decimal number and unit (enter '|' to quit): ";
+        cin >> input >> unit;
+        converted = convert(input, unit);
         
         if (cin.fail()) {
             cout << "Exiting\n";
             status = false;
             break;
         }
-        if (find_smallest(input, low)) {
-            low = input;
-            cout << input << ", the smallest so far.\n";
+        if (find_smallest(converted, low)) {
+            low = converted;
+            cout << converted << "cm, the smallest so far.\n";
 
-        } else if (find_largest(input, high)) {
-            high = input;
-            cout << input << ", the largest so far.\n";
+        } else if (find_largest(converted, high)) {
+            high = converted;
+            cout << converted << "cm, the largest so far.\n";
         } else {
-            cout << input << "\n";
+            cout << converted << "cm.\n";
         }
         
         
