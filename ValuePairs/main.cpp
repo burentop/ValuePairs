1d66bf87ee431f4a63d735a643171318b9425e2b
Brent Perry <burentoperii@gmail.com>
2016-07-08 11:49:28 -0500

    Add tracking for small, large, total and count; and print summary.

--- ValuePairs/main.cpp
+++ ValuePairs/main.cpp
@@ -25,23 +25,30 @@ bool find_largest(double x, double y)
 }
 
 double convert(double input, string unit) {
-    constexpr double inch_to_cm = 2.54;
-    constexpr double meter_to_cm = 100.0;
-    constexpr double feet_to_cm = 30.54;
+    constexpr double cm_to_meter = 0.01;
+    constexpr double inch_to_meter = 0.0254;
+    constexpr double feet_to_meter = 0.3048;
     
     double new_input = 0;
     
     if (unit == "in") {
-        new_input = input * inch_to_cm;
-    } else if (unit == "m") {
-        new_input = input * meter_to_cm;
+        new_input = input * inch_to_meter;
+    } else if (unit == "cm") {
+        new_input = input * cm_to_meter;
     } else if (unit == "ft") {
-        new_input = input * feet_to_cm;
+        new_input = input * feet_to_meter;
     } else {
         new_input = input;
     }
     
     return new_input;
+}
+
+void print_summary(double low, double high, double sum, int count) {
+    cout << "Total values entered: " << count << ".\n";
+    cout << "Sum of values entered: " << sum << " meters.\n";
+    cout << "Smallest value entered: " << low << " meters.\n";
+    cout << "Largest value entered: " << high << " meters.\n";
 }
 
 
@@ -50,6 +57,8 @@ int main()
     double input, converted;
     string unit;
     double low, high;
+    int count;
+    double total = 0;
     bool status = true;
     
     
@@ -57,14 +66,17 @@ int main()
     cout << "Please enter a decimal number and a unit ['cm', 'm', 'in', 'ft'](enter '|' to quit): ";
     cin >> input >> unit;
     if (cin.fail() || (unit != "cm" && unit != "m" && unit != "in" && unit != "ft")) {
-        cout << "Exiting\n";
         status = false;
+        print_summary(low, high, total, count);
     }
     
     converted = convert(input, unit);
+    ++count;
+    total += converted;
+    
     
     low = high = converted;
-    cout << input << unit << " is " << converted << "cm, the only number entered so far.\n";
+    cout << input << unit << " is " << converted << " meters, the only number entered so far.\n";
 
     
     while (status) {
@@ -73,20 +85,22 @@ int main()
         converted = convert(input, unit);
         
         if (cin.fail() || (unit != "cm" && unit != "m" && unit != "in" && unit != "ft")) {
-            cout << "Exiting\n";
             status = false;
+            print_summary(low, high, total, count);
             break;
         }
         if (find_smallest(converted, low)) {
             low = converted;
-            cout << converted << "cm, the smallest so far.\n";
+            cout << converted << " meters, the smallest so far.\n";
 
         } else if (find_largest(converted, high)) {
             high = converted;
-            cout << converted << "cm, the largest so far.\n";
+            cout << converted << " meters, the largest so far.\n";
         } else {
-            cout << converted << "cm.\n";
+            cout << converted << " meters.\n";
         }
+        ++count;
+        total += converted;
         
         
     }
